33c1d959d7e86232a0f115deb47ae6c3158a2d4e
Matthew Dacanay <mattdack@uw.edu>
2022-12-11 23:46:38 -0800

    Minor styling to resume page.

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -5,8 +5,9 @@ import CV from "../../assets/Dacanay-CV-2022.pdf"
 function Resume() {
   return (
     <div className="Resume backgroundImage h-[85%]  border-y-4 border-white overflow-auto grid grid-cols-1 place-items-center">
-      <div>
+      <div className="text-center bg-teal-100/70 border-4 border-slate-400 rounded-md shadow-lg shadow-black p-4">
         <h2>Technical Proficiencies</h2>
+        <br/>
         <p>Languages: HTML, CSS, JavaScript</p>
         <p>Databases: MySQL, MongoDB, GraphQL</p>
         <p>CSS Frameworks: Bootsrap and Tailwind</p>
