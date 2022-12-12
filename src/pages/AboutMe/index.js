eda2f1d240f11069916ca13d7df93186a84dff8e
Matthew Dacanay <mattdack@uw.edu>
2022-12-11 23:44:41 -0800

    Creates button to download resume.

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -37,7 +37,7 @@ function AboutMe() {
             from the University of Washington - Trilogy full time Bootcamp.
             During that course, I created a foundational knowledge base for data
             storage, restful API's and web servers, progressive web
-            applications, and React. In addition, I am familiar with Mongo, Sql,
+            applications, and React.js. In addition, I am familiar with Mongo, Sql,
             and GraphQL database structures. I am excited to continue growing
             and applying this new skillset.
           </p>
@@ -56,7 +56,7 @@ function AboutMe() {
           <br />
           <p className="text-center bg-teal-100/70 border-4 border-slate-400 rounded-md shadow-lg shadow-black p-4">
             In addition to pursuing a career as a software developer. I am an
-            avid hiker, board-game competitor, and video-game enthusiast,
+            avid hiker, board-game competitor, and video-game enthusiast.
           </p>
         </div>
       </div>
--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import CV from "../../assets/Dacanay-CV-2022.pdf"
+
 
 function Resume() {
   return (
     <div className="Resume backgroundImage h-[85%]  border-y-4 border-white overflow-auto grid grid-cols-1 place-items-center">
-      
+      <div>
+        <h2>Technical Proficiencies</h2>
+        <p>Languages: HTML, CSS, JavaScript</p>
+        <p>Databases: MySQL, MongoDB, GraphQL</p>
+        <p>CSS Frameworks: Bootsrap and Tailwind</p>
+        <p>User Interfaces: Handlebars.js and React.js</p>
+      </div>
+      <a href = {CV} download="Dacanay-CV-2022.pdf" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Download Resume</a>
     </div>
   );
 }
